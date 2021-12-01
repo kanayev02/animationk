7a5d8323477df6ca886b3ce61826ad6088648908
КанаевДС@ngknn.local <@ngknn.local>
2021-12-01 14:29:29 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            InitializeComponent();
+
+            DoubleAnimation buttonAnimation = new DoubleAnimation();
+            buttonAnimation.From = helloButton.ActualWidth;
+            buttonAnimation.To = 150;
+            buttonAnimation.Duration = TimeSpan.FromSeconds(5);
+            buttonAnimation.Completed += ButtonAnimation_Completed;
+            helloButton.BeginAnimation(Button.WidthProperty, buttonAnimation);
+            buttonAnimation.Duration = TimeSpan.FromSeconds(3);
+            buttonAnimation.RepeatBehavior = new RepeatBehavior(TimeSpan.FromSeconds(7));
+            
+            
+            ColorAnimation BA = new ColorAnimation(); // создание объекта для настройки анимации
+            ColorConverter CC = new ColorConverter(); // создание объекта для конвертации кода в цвет
+            Color Cstart = (Color)CC.ConvertFrom("#ff0000"); // присваивание начального цвета эл-ту
+            tbavto.Background = new SolidColorBrush(Cstart); // закрашивание эл-та сплошным цветом
+            BA.From = Cstart; // начальное значение свойства
+            BA.To = (Color)CC.ConvertFrom("#00ff00"); // конечное значение свойства
+            BA.Duration = TimeSpan.FromSeconds(5);
+            tbavto.Background.BeginAnimation(SolidColorBrush.ColorProperty, BA);
+
+            
+
+        }
+
+        private void ButtonAnimation_Completed(object sender, EventArgs e)
+        {
+            MessageBox.Show("Анимация завершена");
+        }
+    }
+
+}
